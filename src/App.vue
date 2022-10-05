555ff6eaeaa5c5ea5e047ea0f99028f1ca772e8d
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-10-05 17:48:50 +0200

    create first section main

--- src/App.vue
+++ src/App.vue
@@ -2,24 +2,25 @@
   <div id="app">
     <NavComponent />
     <HeaderComponent />
+    <MainComponent />
   </div>
 </template>
 
 <script>
 import NavComponent from "@/components/NavComponent.vue";
 import HeaderComponent from "@/components/HeaderComponent.vue";
+import MainComponent from "@/components/MainComponent.vue";
 
 export default {
   name: "App",
   components: {
     NavComponent,
     HeaderComponent,
+    MainComponent,
   },
 };
 </script>
 
 <style lang="scss">
 @import "@/assets/style/standard.scss";
-#app {
-}
 </style>

7c4dbfbd09e5b81d5ab1f966a6d165ebd9643d77
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-10-05 16:06:32 +0200

    create header

--- src/App.vue
+++ src/App.vue
@@ -21,11 +21,5 @@ export default {
 <style lang="scss">
 @import "@/assets/style/standard.scss";
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,5 +1,18 @@
 <template>
-  <header></header>
+  <header>
+    <div id="container">
+      <div id="immagini">
+        <img src="../assets/img/slider_slide3_img2.png" alt="" />
+        <img src="../assets/img/slider_slide3_img3.png" alt="" />
+      </div>
+      <div id="barra">
+        <div>
+          <h2>How to Enroll Your Child to a Class?</h2>
+          <button><a href="#">LEARN MORE ></a></button>
+        </div>
+      </div>
+    </div>
+  </header>
 </template>
 
 <script>
@@ -9,4 +22,40 @@ export default {
 </script>
 
 <style scoped lang="scss">
+#container {
+  background-image: url(../assets/img/slider_slide2_background.png);
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 400px;
+  #immagini {
+    display: flex;
+    img {
+      width: 150px;
+      padding-top: 250px;
+      margin-left: 300px;
+    }
+  }
+  #barra {
+    background-image: url(../assets/img/pattern.png);
+    height: 80px;
+    color: #ffffff;
+    div {
+      margin: 0 auto;
+      max-width: 800px;
+      display: flex;
+      justify-content: space-between;
+      align-items: baseline;
+      padding-top: 20px;
+      button {
+        border: 2px solid #ffffff;
+        padding: 10px;
+        font-size: 0.8em;
+      }
+    }
+    // div > h2,
+    // div > button {
+    //   line-height: 80px;
+    // }
+  }
+}
 </style>

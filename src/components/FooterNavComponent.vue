9e8df5eabe40acdcef0131ec7457b3777f5170b1
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-10-07 22:03:23 +0200

    add closing p

--- src/components/FooterNavComponent.vue
+++ src/components/FooterNavComponent.vue
@@ -1,8 +1,11 @@
 <template>
   <nav>
-    <div id="contenitore-icone" v-for="(card, index) in cards" :key="index">
-      <img :src="require(`@/assets/img/${card.immagine}`)" alt="" />
+    <div id="contenitore">
+      <div id="contenitore-icone" v-for="(card, index) in cards" :key="index">
+        <img :src="require(`@/assets/img/${card.immagine}`)" alt="" />
+      </div>
     </div>
+    <p>© Fable - Children Kindergarten WordPress Theme by QuanticaLabs</p>
   </nav>
 </template>
 
@@ -16,7 +19,7 @@ export default {
 </script>
 
 <style scoped lang="scss">
-nav {
+#contenitore {
   display: flex;
   justify-content: center;
   gap: 5px;
@@ -26,5 +29,10 @@ nav {
   background-color: #fe6500;
   width: 40px;
   height: 40px;
+}
+nav p {
+  text-align: center;
+  padding: 30px 0px;
+  color: #999594;
 }
 </style>
